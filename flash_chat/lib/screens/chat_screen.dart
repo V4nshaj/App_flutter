2e0c34848a96dae044729af4f5d78de3cb12b239
V4nshaj <95959007+V4nshaj@users.noreply.github.com>
2023-09-13 22:14:37 +0530

    added cloud storage

--- flash_chat/lib/screens/chat_screen.dart
+++ flash_chat/lib/screens/chat_screen.dart
@@ -1,8 +1,8 @@
-import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'Chat_screen';
@@ -12,7 +12,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
+  String? messageText;
   @override
   void initState() {
     super.initState();
@@ -33,6 +35,23 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+/*
+  void getMsg() async {//to get msg form cloud butt here we need to trigger or wait for the database to load
+    final message = await _firestore.collection('Messages').get();
+    for (var msg in message.docs) {
+      print(msg.data());
+    }
+  }
+  */
+  void msgStream() async {
+    //the snapshot enables to load the messages as soon as they are sent
+    await for (var snapsot in _firestore.collection('Messages').snapshots()) {
+      for (var msg in snapsot.docs) {
+        print(msg.data());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,9 +62,10 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.close),
               onPressed: () {
                 //Implement logout functionality
-                _auth.signOut();
-                Navigator.pushNamedAndRemoveUntil(
-                    context, WelcomeScreen.id, (Route<dynamic> route) => false);
+                msgStream();
+                // _auth.signOut();
+                // Navigator.pushNamedAndRemoveUntil(
+                //     context, WelcomeScreen.id, (Route<dynamic> route) => false);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -65,6 +85,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     child: TextField(
                       onChanged: (value) {
                         //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
@@ -72,6 +93,11 @@ class _ChatScreenState extends State<ChatScreen> {
                   TextButton(
                     onPressed: () {
                       //Implement send functionality.
+                      _firestore.collection('Messages').add({
+                        //sends messages
+                        'text': messageText,
+                        'sender': logedinuser!.email,
+                      });
                     },
                     child: Text(
                       'Send',
